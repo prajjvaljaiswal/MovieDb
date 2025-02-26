5655c047a6f97e7130d93ed0b0fffe15409c986d
PRAJJVAL JAISWAL <prajjaiswal158@gmail.com>
2025-02-26 23:34:34 +0530

    - change filles

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import { Button } from './components/ui/button'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      Hell world
+    </div>
+  )
+}
+
+export default App
