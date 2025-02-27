3f4a2579160652643632ab3a62cac5813dec06d8
PRAJJVAL JAISWAL <prajjaiswal158@gmail.com>
2025-02-27 16:34:36 +0530

    - bug fix

--- src/components/pages/Browse.tsx
+++ src/components/pages/Browse.tsx
@@ -11,18 +11,18 @@ const Browse = () => {
   const movies: Movie[] = useSelector(
     (state: RootState) => state.movie.popularMovies
   );
-  if (!movies || movies.length === 0) {
-    return (
-      <div className="min-h-screen bg-zinc-900 text-white ">
-        <Header />
-        <div className="flex justify-center items-center">
-          <p className="text-lg text-zinc-400">
-            No movies available of this name.
-          </p>
-        </div>
-      </div>
-    );
-  }
+  // if (!movies || movies.length === 0) {
+  //   return (
+  //     <div className="min-h-screen bg-zinc-900 text-white ">
+  //       <Header />
+  //       <div className="flex justify-center items-center">
+  //         <p className="text-lg text-zinc-400">
+  //           No movies available of this name.
+  //         </p>
+  //       </div>
+  //     </div>
+  //   );
+  // }
   if (!name) return;
   useSearchMovies(name);
   return (
@@ -30,7 +30,7 @@ const Browse = () => {
       <Header />
       <main className="container mx-auto px-4 py-8">
         <div className="grid grid-cols-2 gap-6 md:grid-cols-4">
-          {movies.map((movie) => (
+          {movies && movies.map((movie) => (
             <MovieCard key={movie.id} movie={movie} />
           ))}
         </div>
