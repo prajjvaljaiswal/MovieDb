b2e0392972dd552ff2e67587fa6cdf1be588ab6f
PRAJJVAL JAISWAL <prajjaiswal158@gmail.com>
2025-02-27 16:37:17 +0530

    - error fix

--- src/components/pages/error.tsx
+++ src/components/pages/error.tsx
@@ -5,7 +5,7 @@ const Error = ()=> {
     const error = useRouteError();
 
     return(
-        <div>
+        <div className="min-h-screen bg-zinc-900 text-white flex justify-center items-center">
             <h1>OPPS!</h1>
             <h2>{isRouteErrorResponse(error)
           ? `${error.status} : ${error.statusText}`
